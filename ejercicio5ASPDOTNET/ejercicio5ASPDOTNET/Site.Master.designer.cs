520f1a7fc6a9707f94f876b344447aa4f2a476bd
Xander313 <jhonn.roldan8626@utc.edu.ec>
2025-06-09 19:03:15 -0500

    se ha cargado la palntilla por completo

--- ejercicio5ASPDOTNET/ejercicio5ASPDOTNET/Site.Master.designer.cs
+++ ejercicio5ASPDOTNET/ejercicio5ASPDOTNET/Site.Master.designer.cs
@@ -15,15 +15,6 @@ namespace ejercicio5ASPDOTNET
     {
 
         /// <summary>
-        /// Control ContentPlaceHolder1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
-
-        /// <summary>
         /// Control MainContent.
         /// </summary>
         /// <remarks>
